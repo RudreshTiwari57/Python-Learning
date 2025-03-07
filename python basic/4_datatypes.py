6569a2f1d58210ddb2a5c0015b7c3ad590ebef09
DESKTOP-16Q7AG9\Rudresh Tiwari <rudreshtiwari57@gmail.com>
2025-03-07 20:18:15 +0530

    data typrs

---
+++ python basic/4_datatypes.py
@@ -0,0 +1,27 @@
+# Built-in Data Types
+    # In programming, data type is an important concept.
+    # Variables can store data of different types, and different types can do different things.
+    # Python has the following data types built-in by default, in these categories:
+#
+# Text Type:	str
+    # Numeric Types:	int
+#                           x = 5
+#                           print(type(x),x)
+#                       float
+#                           x = 5.34
+#                           print(type(x),x)
+#                       complex
+#                           x = 1j
+#                           print(type(x),x)
+
+    # Boolean Type:	bool
+#                           x = True
+#                           print(type(x),x)
+#                           x = False
+#                           print(type(x),x)
+
+    # None Type:	NoneType
+#                           x = None
+#                           print(type(x),x)
+
+
